f0c5c2bd6762ed7fdd8f3b77f61a1c8aee28aed5
Shashanksowri <shashank.sowri@siemens.com>
2022-12-13 13:01:53 +0530

    second commit

--- app/src/main/java/com/example/sampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/sampleapplication/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
